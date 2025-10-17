0a090775b8839ca46419ee3bb88707acee5ccff0
Luis <luisparadela@z1.digital>
2025-10-17 13:08:45 +0100

    Create views

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    """Muestra todos los posts publicados"""
+    posts = Post.objects.filter(is_published=True).order_by('-created_at')
+    print("📜 POSTS CARGADOS:", posts)
+    return render(request, 'core/post_list.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'core/post_detail.html', {'post': post})
