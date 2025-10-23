0f6657e55d76ed6fdf2f74ceadc125097f190223
Luis <luisparadela@z1.digital>
2025-10-23 11:02:03 +0100

    test: add more tests

--- mysite/core/tests.py
+++ mysite/core/tests.py
@@ -1,10 +1,14 @@
-from django.test import TestCase
+# core/tests.py
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 from .models import Blog, Post, Tag
 
 class BlogModelTest(TestCase):
-    """Test basic Blog model functionality"""
+    """Test Blog model functionality"""
     
     def setUp(self):
         """Set up test data"""
@@ -35,7 +39,7 @@ class BlogModelTest(TestCase):
         self.assertEqual(str(blog), expected)
 
 class TagModelTest(TestCase):
-    """Test basic Tag model functionality"""
+    """Test Tag model functionality"""
     
     def test_tag_creation(self):
         """Test that a tag can be created"""
@@ -54,7 +58,7 @@ class TagModelTest(TestCase):
             Tag.objects.create(name='Django')
 
 class PostModelTest(TestCase):
-    """Test basic Post model functionality"""
+    """Test Post model functionality"""
     
     def setUp(self):
         """Set up test data"""
@@ -113,3 +117,132 @@ class PostModelTest(TestCase):
         self.assertEqual(post.tags.count(), 2)
         self.assertIn(tag1, post.tags.all())
         self.assertIn(tag2, post.tags.all())
+
+class APITestCase(APITestCase): 
+    """Test API endpoints"""
+    
+    def setUp(self):
+        """Set up test data"""
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpass123'
+        )
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.blog = Blog.objects.create(
+            user=self.user,
+            title='Test Blog'
+        )
+    
+    def test_user_registration(self):
+        """Test user registration endpoint"""
+        data = {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'newpass123',
+            'password_confirm': 'newpass123'
+        }
+        response = self.client.post('/api/users/register/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('token', response.data)
+    
+    def test_user_login(self):
+        """Test user login endpoint"""
+        data = {
+            'username': 'testuser',
+            'password': 'testpass123'
+        }
+        response = self.client.post('/api/users/login/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+    
+    def test_post_creation(self):
+        """Test post creation endpoint"""
+        data = {
+            'title': 'Test Post',
+            'content': '<p>Test content</p>',
+            'excerpt': 'Test excerpt',
+            'is_published': True
+        }
+        response = self.client.post('/api/posts/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'Test Post')
+    
+    def test_post_list(self):
+        """Test post list endpoint"""
+        Post.objects.create(
+            blog=self.blog,
+            title='Test Post',
+            content='<p>Test content</p>'
+        )
+        response = self.client.get('/api/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+    
+    def test_post_permissions(self):
+        """Test that users can only modify their own posts"""
+        # Create another user
+        other_user = User.objects.create_user(
+            username='otheruser',
+            email='other@example.com',
+            password='otherpass123'
+        )
+        other_blog = Blog.objects.create(
+            user=other_user,
+            title='Other Blog'
+        )
+        other_post = Post.objects.create(
+            blog=other_blog,
+            title='Other Post',
+            content='<p>Other content</p>'
+        )
+        
+        # Try to update other user's post
+        data = {'title': 'Updated Title'}
+        response = self.client.put(f'/api/posts/{other_post.id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_tag_creation(self):
+        """Test tag creation endpoint"""
+        data = {'name': 'Django'}
+        response = self.client.post('/api/tags/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Django')
+    
+    def test_published_posts_filter(self):
+        """Test published posts filter"""
+        # Create published and unpublished posts
+        Post.objects.create(
+            blog=self.blog,
+            title='Published Post',
+            content='<p>Published content</p>',
+            is_published=True
+        )
+        Post.objects.create(
+            blog=self.blog,
+            title='Unpublished Post',
+            content='<p>Unpublished content</p>',
+            is_published=False
+        )
+        
+        response = self.client.get('/api/posts/published/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Published Post')
+    
+    def test_posts_by_tag_filter(self):
+        """Test posts by tag filter"""
+        tag = Tag.objects.create(name='Django')
+        post = Post.objects.create(
+            blog=self.blog,
+            title='Django Post',
+            content='<p>Django content</p>'
+        )
+        post.tags.add(tag)
+        
+        response = self.client.get('/api/posts/by_tag/?tag=Django')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Django Post')
